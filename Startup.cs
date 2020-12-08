a39036f8be8f51864b698644fcbbe57f7fc50684
Kate-max <tabolskaya.kate.2001@yandex.ru>
2020-12-08 13:00:05 +0300

    Сервер

--- Startup.cs
+++ Startup.cs
@@ -10,8 +10,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-
 using System.Text;
 using System.Threading;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
